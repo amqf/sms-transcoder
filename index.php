30a1df7baae50e56108847d065b0654c52f8915c
Antônio M. Quadros Filho <antoniomquadrosfilho@gmail.com>
2022-06-29 01:15:30 -0300

    fix: pass correct transcoder arguments

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 use SMSTranscoder\Decoders\GSMDecoder;
+use SMSTranscoder\Encoders\JSONEncoder;
 use SMSTranscoder\Transcoder;
 
 require_once "vendor/autoload.php";
 
 echo (new Transcoder(
-    GSMDecoder::make(file_get_contents('php://stdin')),
-    
+    GSMDecoder::make('php://stdin'),
+    new JSONEncoder
 ))->transcode();
